7f23230ddf2c4231b66d389c610421231b6536eb
Fabian Aichele <fabian.aichele@heitec-pts.de>
2026-02-07 19:07:37 +0100

    Initial commit.
    # Conflicts:
    #	README.md
    #	pyproject.toml

---
+++ fastapi_celery_manager/__init__.py
@@ -0,0 +1,18 @@
+"""fastapi_celery_manager
+
+Reusable FastAPI routers for basic Celery administration.
+
+Public API:
+- get_celery_admin_router: REST endpoints under /api/celery
+- get_celery_admin_ui_router: HTML page rendering celery_monitor.html
+"""
+
+from .celery_admin import get_celery_admin_router
+from .ui import get_celery_admin_ui_router
+from .ui import get_celery_admin_ui_router_with_login_redirect
+
+__all__ = [
+    "get_celery_admin_router",
+    "get_celery_admin_ui_router",
+    "get_celery_admin_ui_router_with_login_redirect"
+]

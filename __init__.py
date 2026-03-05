04b14ccc49d538ed38042231100e38afd8879130
Fabian Aichele <fabian.aichele@heitec-pts.de>
2026-03-05 14:40:45 +0100

    Removed duplicate files.

--- __init__.py
+++ __init__.py
@@ -7,9 +7,9 @@ Public API:
 - get_celery_admin_ui_router: HTML page rendering celery_monitor.html
 """
 
-from .celery_admin import get_celery_admin_router
-from .ui import get_celery_admin_ui_router
-from .ui import get_celery_admin_ui_router_with_login_redirect
+from .fastapi_celery_task_manager import get_celery_admin_router
+from .fastapi_celery_task_manager.ui import get_celery_admin_ui_router
+from .fastapi_celery_task_manager.ui import get_celery_admin_ui_router_with_login_redirect
 
 __all__ = [
     "get_celery_admin_router",
